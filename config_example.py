e1dd4ce1c7c2e680383717016ac79626b22c05a5
Jesse Rodriguez <33294331+jessearodriguez@users.noreply.github.com>
2023-03-25 02:08:32 -0700

    Create config_example.py

---
+++ config_example.py
@@ -0,0 +1,33 @@
+inputtext = ""
+#https://huggingface.co/docs/api-inference/detailed_parameters for detailed parameter explenation: these were found to be decent for LLAMA-13b-4bit
+maxtok = 100
+temperature = .41
+top_p = 1
+typical_p= 1
+repetition_penalty = 1.1
+top_k = 50
+min_length = 5
+no_repeat_ngram = 0
+num_beams = 1
+penalty_alpha = 0
+length_penalty = 1
+early_stopping = True
+
+chatter_name = ""
+ai_name = ""
+
+context = f""
+
+
+example_dialogue = f""
+memory = 10
+
+
+
+sendport = 9000
+rescieveport = 9001
+ip = ""
+
+tts_wpm = 170
+
+
